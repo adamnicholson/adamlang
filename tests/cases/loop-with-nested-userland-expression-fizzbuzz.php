d484e56c87ffa4922e03ba7a5dbdc6c6cedae5ae
Adam Nicholson <adamnicholson10@gmail.com>
2016-09-24 17:40:55 +0100

    Improve test

--- tests/cases/loop-with-nested-userland-expression-fizzbuzz.php
+++ tests/cases/loop-with-nested-userland-expression-fizzbuzz.php
@@ -3,8 +3,8 @@ $code = <<<ADAMLANG
 fn "factor-of-3?" "num" {equals 0 (mod :num 3)}
 fn "factor-of-5?" "num" {equals 0 (mod :num 5)}
 fn "factor-of-3-and-5?" "num" {perhaps (factor-of-5? :num) {factor-of-3? :num} false}
-fn "fizzbuzz" "num" {perhaps (factor-of-5? :num) "buzz" {perhaps (factor-of-3? :num) "fizz" :num}}
-loop 1..9 {print (str (fizzbuzz :i)) EOL}
+fn "fizzbuzz" "num" {perhaps (factor-of-3-and-5? :num) "fizzbuzz" {perhaps (factor-of-5? :num) "buzz" {perhaps (factor-of-3? :num) "fizz" :num}}}
+loop 1..30 {print (str (fizzbuzz :i)) EOL}
 ADAMLANG;
 $output = <<<TEXT
 1
@@ -16,5 +16,26 @@ fizz
 7
 8
 fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+16
+17
+fizz
+19
+buzz
+fizz
+22
+23
+fizz
+buzz
+26
+fizz
+28
+29
+fizzbuzz
 
 TEXT;
