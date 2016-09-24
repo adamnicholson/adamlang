5892cab6a8b65f0cd26964e1d32588aed41362f1
Adam Nicholson <adamnicholson10@gmail.com>
2016-09-24 15:58:44 +0100

    Improve debug messages

--- src/Lexer.php
+++ src/Lexer.php
@@ -54,21 +54,26 @@ class Lexer
     private function readTilLayerEnds($open, $close)
     {
         $value = "";
-        $layers = 0;
-        while (true) {
-            if ($this->stream->peek() === $open) {
-                $layers++;
-            }
-            if ($this->stream->peek() === $close) {
-                if ($layers === 0) {
-                    break;
-                }
-                $layers--;
-            }
 
-            $value .= $this->stream->read();
+        try {
+            $layers = 0;
+            while (true) {
+                if ($this->stream->peek() === $open) {
+                    $layers++;
+                }
+                if ($this->stream->peek() === $close) {
+                    if ($layers === 0) {
+                        break;
+                    }
+                    $layers--;
+                }
+
+                $value .= $this->stream->read();
+            }
+            return $value;
+        } catch (\OutOfBoundsException $e) {
+            throw new \OutOfBoundsException("Unexpected EOF when trying to find closing $close symbol after '$value'");
         }
-        return $value;
     }
 
     private function readTilPatternOrEof($pattern)
@@ -90,98 +95,103 @@ class Lexer
      */
     private function getNext()
     {
-        switch ($this->previous->getType()) {
+        try {
+            switch ($this->previous->getType()) {
 
-            case Token::T_BOF:
-            case Token::T_EOL:
-                return new Token(Token::T_BOL);
+                case Token::T_BOF:
+                case Token::T_EOL:
+                    return new Token(Token::T_BOL);
 
-            case Token::T_BOL:
-                $value = "";
-                while (true) {
-                    if ($this->stream->ended() || $this->stream->peek() === " ") {
-                        break;
+                case Token::T_BOL:
+                    $value = "";
+                    while (true) {
+                        if ($this->stream->ended() || $this->stream->peek() === " ") {
+                            break;
+                        }
+                        $value .= $this->stream->read();
                     }
-                    $value .= $this->stream->read();
-                }
-                return new Token(Token::T_FUNCTION, $value);
+                    return new Token(Token::T_FUNCTION, $value);
 
-            case Token::T_FUNCTION:
+                case Token::T_FUNCTION:
 
-                if ($this->stream->ended()) {
-                    return new Token(Token::T_EOF);
-                }
+                    if ($this->stream->ended()) {
+                        return new Token(Token::T_EOF);
+                    }
 
-                if ($this->stream->peek() === " ") {
-                    return new Token(Token::T_FUNCTION_ARG_SEPARATOR, $this->stream->read());
-                }
+                    if ($this->stream->peek() === " ") {
+                        return new Token(Token::T_FUNCTION_ARG_SEPARATOR, $this->stream->read());
+                    }
 
-                throw new \RuntimeException("Expected EOF or ARG_SEPARATOR");
+                    throw new \RuntimeException("Expected EOF or ARG_SEPARATOR");
 
-            case Token::T_FUNCTION_ARG_SEPARATOR:
-                if ($this->stream->peek() === '"') {
-                    $this->stream->read(); // "
-                    $token = new Token(Token::T_STRING_LITERAL, $this->readTil('"'));
-                    $this->stream->read(); // "
-                    return $token;
-                }
+                case Token::T_FUNCTION_ARG_SEPARATOR:
+                    if ($this->stream->peek() === '"') {
+                        $this->stream->read(); // "
+                        $token = new Token(Token::T_STRING_LITERAL, $this->readTil('"'));
+                        $this->stream->read(); // "
+                        return $token;
+                    }
 
-                if ($this->stream->peek() === ':') {
-                    $this->stream->read(); // :
-                    return new Token(Token::T_VALUE_REFERENCE, $this->readTilPatternOrEof('/[\s\n]/'));
-                }
+                    if ($this->stream->peek() === ':') {
+                        $this->stream->read(); // :
+                        return new Token(Token::T_VALUE_REFERENCE, $this->readTilPatternOrEof('/[\s\n]/'));
+                    }
 
-                if (preg_match("/[0-9]/", $this->stream->peek())) {
-                    return new Token(Token::T_INTEGER, (int) $this->readTilPatternOrEof('/[\s\n]/'));
-                }
+                    if (preg_match("/[0-9]/", $this->stream->peek())) {
+                        return new Token(Token::T_INTEGER, (int) $this->readTilPatternOrEof('/[\s\n]/'));
+                    }
 
-                if ($this->stream->peek() === '{') {
-                    $this->stream->read(); // (
-                    $value = $this->readTilLayerEnds('{', '}');
-                    $token = new Token(Token::T_INLINE_EXPRESSION, new Lexer(new InMemoryIO($value)));
-                    $this->stream->read(); // )
-                    return $token;
-                }
+                    if ($this->stream->peek() === '{') {
+                        $this->stream->read(); // (
+                        $value = $this->readTilLayerEnds('{', '}');
+                        $token = new Token(Token::T_INLINE_EXPRESSION, new Lexer(new InMemoryIO($value)));
+                        $this->stream->read(); // )
+                        return $token;
+                    }
 
-                if ($this->stream->peek() === '(') {
-                    $this->stream->read(); // (
-                    $value = $this->readTilLayerEnds('(', ')');
-                    $token = new Token(Token::T_EXPRESSION, new Lexer(new InMemoryIO($value)));
-                    $this->stream->read(); // )
-                    return $token;
-                }
+                    if ($this->stream->peek() === '(') {
+                        $this->stream->read(); // (
+                        $value = $this->readTilLayerEnds('(', ')');
+                        $token = new Token(Token::T_EXPRESSION, new Lexer(new InMemoryIO($value)));
+                        $this->stream->read(); // )
+                        return $token;
+                    }
 
-                return new Token(Token::T_CONSTANT, $this->readTilPatternOrEof('/[\s\n]+/'));
-                break;
+                    return new Token(Token::T_CONSTANT, $this->readTilPatternOrEof('/[\s\n]+/'));
+                    break;
 
-            case Token::T_FUNCTION_ARG:
-                return new Token(Token::T_EOL, $this->stream->read());
-                break;
-
-            case Token::T_EXPRESSION:
-            case Token::T_CONSTANT:
-            case Token::T_INLINE_EXPRESSION:
-            case Token::T_VALUE_REFERENCE:
-            case Token::T_STRING_LITERAL:
-            case Token::T_INTEGER:
-            case Token::T_FUNCTION_ARG:
-                if ($this->stream->ended()) {
-                    return new Token(Token::T_EOF);
-                }
-
-                $nextChar = $this->stream->peek();
-
-                if ($nextChar === "\n") {
+                case Token::T_FUNCTION_ARG:
                     return new Token(Token::T_EOL, $this->stream->read());
-                } elseif ($nextChar === " ") {
-                    return new Token(Token::T_FUNCTION_ARG_SEPARATOR, $this->stream->read());
-                } else {
-                    throw new \RuntimeException("Expecting EOL or ARG_SEPARATOR, got " . ord($nextChar));
-                }
+                    break;
 
-            default:
-                throw new \RuntimeException("Nothing can follow a " . $this->previous->getType());
+                case Token::T_EXPRESSION:
+                case Token::T_CONSTANT:
+                case Token::T_INLINE_EXPRESSION:
+                case Token::T_VALUE_REFERENCE:
+                case Token::T_STRING_LITERAL:
+                case Token::T_INTEGER:
+                case Token::T_FUNCTION_ARG:
+                    if ($this->stream->ended()) {
+                        return new Token(Token::T_EOF);
+                    }
 
+                    $nextChar = $this->stream->peek();
+
+                    if ($nextChar === "\n") {
+                        return new Token(Token::T_EOL, $this->stream->read());
+                    } elseif ($nextChar === " ") {
+                        return new Token(Token::T_FUNCTION_ARG_SEPARATOR, $this->stream->read());
+                    } else {
+                        throw new \RuntimeException("Expecting EOL or ARG_SEPARATOR, got " . ord($nextChar));
+                    }
+
+                default:
+                    throw new \RuntimeException("Nothing can follow a " . $this->previous->getType());
+
+            }
+        } catch (\OutOfBoundsException $e) {
+            throw new \OutOfBoundsException("Unexpected end of file after " . $this->previous->getType(), 0, $e);
         }
+
     }
 }
